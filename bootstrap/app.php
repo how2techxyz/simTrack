2d4bcdb7923cec7fddd99fad3e21af664b442eba
how2techxyz <153798732+how2techxyz@users.noreply.github.com>
2024-04-30 19:39:27 +0500

    Just updating for testing (#14)
    Co-authored-by: tauseefnasir <tauseefnasir501@gmail.com>

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@
 |--------------------------------------------------------------------------
 | Create The Application
 |--------------------------------------------------------------------------
-|
+| just update
 | The first thing we will do is create a new Laravel application instance
 | which serves as the "glue" for all the components of Laravel, and is
 | the IoC container for the system binding all of the various parts.
